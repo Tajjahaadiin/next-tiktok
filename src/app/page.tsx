ebb642f989f40c59c6060bf99a852002b91e3c07
Tajjz <tajjuddiinauliya@gmail.com>
2025-05-17 00:12:06 +0700

    bug: signin and signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
 import SignIn from "./components/sign-in";
 import SignOut from "./components/sign-out";
-import { auth } from "@/auth";
-import { redirect } from "next/dist/client/components/redirect";
-export default async function Home() {
-  const session = await auth();
 
-  if (!session?.user) return redirect("/test");
+export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,8 @@
-export default function test() {
+import { auth } from "@/auth";
+import { redirect } from "next/dist/server/api-utils";
+export default async function test() {
+  const session = await auth();
+
+  if (!session?.user) return redirect;
   return <>Test</>;
 }
